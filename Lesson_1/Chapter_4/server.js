4f5a0ab90725f16d6f2dea7756c9a9d98b36b5ce
Martin Bing <bing.martin.photography@gmail.com>
2014-12-08 10:50:02 +0100

    Update changes to example in chapter

--- Lesson_1/Chapter_4/server.js
+++ Lesson_1/Chapter_4/server.js
@@ -1,8 +1,11 @@
 var http = require('http');
 var server = http.createServer(function (req, res) {
-	var body = 'Hello World';
+	var url = 'http://thinkful.com';
+	var body = '<span>Redirecting to <a href="' + url + '">' + url + '</a></span>';
+	res.setHeader('Location', url);
+	res.statusCode = 302;
 	res.setHeader('Content-Length', body.length);
-	res.setHeader('Content-Type', 'text/plain');
+	res.setHeader('Content-Type', 'text/html');
 	res.end(body);
 });
 server.listen(9000, function () {
